354fc0eff8ec0d55ca94da746bdba462f8a93673
Ahmed <ahmed.mano.am68@gmail.com>
2021-08-05 14:21:55 +0200

    initializing the project and stucturing the folder and imported react-icon liberary and created initial data.js

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Navbar from "./Navbar";
+
+const App = () => {
+  return (
+    <>
+      <h2>app componenet</h2>
+      <Navbar />
+    </>
+  );
+};
+
+export default App;
---
+++ src/Navbar.js
@@ -0,0 +1,14 @@
+import React, { useState, useEffect, useRef } from "react";
+import { FaBars, FaTwitter } from "react-icons/fa";
+import { social, links } from "./data";
+import Logo from "./logo.svg";
+
+const Navbar = () => {
+  return (
+    <>
+      <h2>Navbar component</h2>
+    </>
+  );
+};
+
+export default Navbar;

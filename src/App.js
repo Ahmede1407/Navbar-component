374a4041b5670205cffa93503b7b56e1bf45f56d
Ahmed <ahmed.mano.am68@gmail.com>
2021-08-05 20:05:52 +0200

    created Navbar componenet and added initial structure with styling.

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import Navbar from "./Navbar";
 const App = () => {
   return (
     <>
-      <h2>app componenet</h2>
       <Navbar />
     </>
   );
--- src/Navbar.js
+++ src/Navbar.js
@@ -1,13 +1,53 @@
 import React, { useState, useEffect, useRef } from "react";
 import { FaBars, FaTwitter } from "react-icons/fa";
 import { social, links } from "./data";
-import Logo from "./logo.svg";
+import logo from "./logo.svg";
 
 const Navbar = () => {
   return (
-    <>
-      <h2>Navbar component</h2>
-    </>
+    <nav>
+      <div className="nav-center">
+        <div className="nav-header">
+          <img src={logo} alt="logo" />
+          <button className="nav-toggle">
+            <FaBars />
+          </button>
+        </div>
+        <div className="links-container show-container">
+          <ul className="links">
+            <li>
+              <a href="#">home</a>
+            </li>
+            <li>
+              <a href="#">home2</a>
+            </li>
+            <li>
+              <a href="#">home3</a>
+            </li>
+            <li>
+              <a href="#">home4</a>
+            </li>
+          </ul>
+        </div>
+        <ul className="social-icons">
+          <li>
+            <a href="http://www.twitter.com">
+              <FaTwitter />
+            </a>
+          </li>
+          <li>
+            <a href="http://www.twitter.com">
+              <FaTwitter />
+            </a>
+          </li>
+          <li>
+            <a href="http://www.twitter.com">
+              <FaTwitter />
+            </a>
+          </li>
+        </ul>
+      </div>
+    </nav>
   );
 };
 
